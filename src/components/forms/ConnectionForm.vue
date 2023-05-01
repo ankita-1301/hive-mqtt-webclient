98ac3dfc37e10391204955e2563b267c8ce82eed
Ankita Shah <ankita.shah@hypcloud.de>
2023-05-01 10:31:55 +0200

    Connection form

---
+++ src/components/forms/ConnectionForm.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+.connection-form
+  h2.form-title Connection
+  input#hostname(
+    v-model="hostName"
+    name="hostname"
+    placeholder="Hostname"
+  )
+  input#username(
+    v-model="userName"
+    name="username"
+    placeholder="Username"
+  )
+  input#password(
+    v-model="password"
+    name="password"
+    placeholder="Password"
+    type="password"
+  )
+  button(@click="onConnect") Connect
+  .success(v-if="isClientConnected") MQTT webclient is connected!
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const emit = defineEmits(['create-connection'])
+
+const hostName = ref('')
+const userName = ref('')
+const password = ref('')
+
+defineProps({
+  isClientConnected: false,
+})
+
+function onConnect() {
+  emit('create-connection', hostName.value, userName.value, password.value)
+}
+</script>
+
+<style>
+.success {
+  color: green;
+  margin: 0.5rem 0;
+  font-weight: bold;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,2 +1,61 @@
-<template></template>
-<script setup></script>
+<template lang="pug">
+.home-view
+  h1.page-title MQTT WebClient
+  connection-form(
+    :is-client-connected="data.isClientConnected"
+    @create-connection="onConnect"
+  )
+  </template>
+
+<script setup>
+import mqtt from 'mqtt/dist/mqtt'
+import { reactive } from 'vue'
+import { v4 as uuid } from 'uuid'
+import ConnectionForm from '@/components/forms/ConnectionForm.vue'
+
+const data = reactive({
+  isClientConnected: false,
+  mqttClient: {},
+})
+
+function onConnect(hostname, username, password) {
+  const clientOptions = {
+    clean: true,
+    clientId: uuid(),
+    hostname,
+    keepalive: 60,
+    password,
+    path: '/mqtt',
+    port: 8884,
+    protocol: 'wss',
+    protocolVersion: 4,
+    reconnectPeriod: 0,
+    resubscribe: false,
+    username,
+  }
+
+  data.mqttClient = mqtt.connect(hostname, clientOptions)
+
+  console.log('Connecting mqtt client', data.mqttClient)
+
+  data.mqttClient.on('connect', function () {
+    data.isClientConnected = data.mqttClient.connected
+    console.log('Connected..!')
+  })
+
+  data.mqttClient.on('error', (error) => {
+    console.log('Connection error: ', error)
+    data.mqttClient.end()
+  })
+
+  data.mqttClient.on('reconnect', () => {
+    console.log('Reconnecting...')
+  })
+}
+</script>
+
+<style>
+h1 {
+  margin: 2rem auto;
+}
+</style>
